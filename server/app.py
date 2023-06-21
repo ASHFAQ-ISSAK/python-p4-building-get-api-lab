c9076d0acb123eec43279621a4a14689acf339ff
ASHFAQ ISSAK <ashfaq.abdi@gmail.com>
2023-06-21 19:42:31 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -6,33 +6,92 @@ from flask_migrate import Migrate
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return '<h1>Bakery GET API</h1>'
+    return "<h1>Bakery GET API</h1>"
 
-@app.route('/bakeries')
-def bakeries():
-    return ''
 
-@app.route('/bakeries/<int:id>')
-def bakery_by_id(id):
-    return ''
+@app.route("/bakeries", methods=["GET"])
+def get_bakeries():
+    bakeries = Bakery.query.all()
+    bakery_list = []
+    for bakery in bakeries:
+        bakery_data = {
+            "id": bakery.id,
+            "name": bakery.name,
+            # Include other bakery attributes as needed
+        }
+        bakery_list.append(bakery_data)
+    return jsonify(bakery_list)
 
-@app.route('/baked_goods/by_price')
-def baked_goods_by_price():
-    return ''
 
-@app.route('/baked_goods/most_expensive')
-def most_expensive_baked_good():
-    return ''
+from flask import Flask, jsonify, make_response
+from models import Bakery, BakedGood
 
-if __name__ == '__main__':
+
+@app.route("/bakeries/<int:id>", methods=["GET"])
+def get_bakery(id):
+    bakery = Bakery.query.get(id)
+
+    if bakery is None:
+        response = {"error": "Bakery not found"}
+        return jsonify(response), 404
+
+    baked_goods = [baked_good.name for baked_good in bakery.baked_goods]
+
+    bakery_data = {
+        "id": bakery.id,
+        "name": bakery.name,
+        "baked_goods": baked_goods
+        # Include other bakery attributes as needed
+    }
+
+    return jsonify(bakery_data)
+
+
+@app.route("/baked_goods/by_price", methods=["GET"])
+def get_baked_goods_by_price():
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+
+    baked_goods_data = []
+    for baked_good in baked_goods:
+        baked_good_data = {
+            "id": baked_good.id,
+            "name": baked_good.name,
+            "price": baked_good.price,
+            # Include other baked good attributes as needed
+        }
+        baked_goods_data.append(baked_good_data)
+
+    return jsonify(baked_goods_data)
+
+
+@app.route("/baked_goods/most_expensive", methods=["GET"])
+def get_most_expensive_baked_good():
+    most_expensive_baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+
+    if most_expensive_baked_good is None:
+        # Handle the case when no baked goods are available
+        return jsonify({"message": "No baked goods found."}), 404
+
+    baked_good_data = {
+        "id": most_expensive_baked_good.id,
+        "name": most_expensive_baked_good.name,
+        "price": most_expensive_baked_good.price,
+        # Include other baked good attributes as needed
+    }
+
+    return jsonify(baked_good_data)
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -2,19 +2,27 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
 
+
 class Bakery(db.Model, SerializerMixin):
-    __tablename__ = 'bakeries'
+    __tablename__ = "bakeries"
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    baked_goods = db.relationship("BakedGood", backref="bakery", lazy=True)
+
 
 class BakedGood(db.Model, SerializerMixin):
-    __tablename__ = 'baked_goods'
+    __tablename__ = "baked_goods"
 
     id = db.Column(db.Integer, primary_key=True)
-    
+    name = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Integer)
+    bakery_id = db.Column(db.Integer, db.ForeignKey("bakeries.id"), nullable=False)
